2c2596c1afcdd2630d8f48f26c8776e0ee16d023
8devil3 <8dev.il3@gmail.com>
2022-03-15 12:21:42 +0100

    caricata lista contatti

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        user: {
+            name: 'Sofia', 
+            avatar: 'avatar_io.jpg',
+        },
+        directory: [{
+            name: 'Michele',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_1.jpg',
+        },
+        {
+            name: 'Fabio',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_2.jpg',
+        },
+        {
+            name: 'Samuele',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_3.jpg',
+        },
+        {
+            name: 'Aessandro B.',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_4.jpg',
+        },
+        {
+            name: 'Alessandro L.',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_5.jpg',
+        },
+        {
+            name: 'Claudia',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_6.jpg',
+        },
+        {
+            name: 'Federico',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_7.jpg',
+        },
+        {
+            name: 'Davide',
+            lastActivity: 'Ultimo messaggio inviato',
+            lastActivityTime: '12:00',
+            avatar: 'avatar_8.jpg',
+        },
+    ],
+    },
+    methods: {
+
+    }
+});
